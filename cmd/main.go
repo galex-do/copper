32c45501cb096c9b451e7b15634dd21e3e33c61a
galex <alexey.gontarenko@intermobility.ru>
2025-03-14 13:42:19 +0300

    path to pkg

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "copper/pkg/module1"
-    "copper/pkg/module2"
+	"fmt"
+
+	"github.com/galex-do/copper/pkg/module1"
+	"github.com/galex-do/copper/pkg/module2"
 )
 
 func main() {
-    fmt.Println("Initializing Copper Application")
-    
-    module1.DoSomething()
-    module2.DoSomethingElse()
+	fmt.Println("Initializing Copper Application")
+
+	module1.DoSomething()
+	module2.DoSomethingElse()
 }
